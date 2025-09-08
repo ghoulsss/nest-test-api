d279f1937ba7fa4cd23592ad9dca19f243712fa4
Pavlov Danil <danpavkzn@mail.ru>
2025-09-08 16:00:09 +0300

    prettier

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -12,7 +12,6 @@ import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 
-
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
