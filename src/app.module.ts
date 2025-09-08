6666972cc166853877c048e044f466265eacb683
Pavlov Danil <danpavkzn@mail.ru>
2025-09-08 11:18:20 +0300

    чистка

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MoviesController } from './movies/movies.controller';
 import { MoviesModule } from './movies/movies.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [MoviesModule, DatabaseModule],
-  controllers: [AppController, MoviesController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
